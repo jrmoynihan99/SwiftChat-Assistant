7518feb766ca51678e44ab086635b0ca03ce3eea
Jason Moynihan <jasonmoynihan@Jasons-MacBook-Air.local>
2025-10-15 17:22:29 -0400

    Initial xcode project creation

---
+++ SwiftChat Assistant/SwiftChat AssistantTests/SwiftChat_AssistantTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChat_AssistantTests.swift
+//  SwiftChat AssistantTests
+//
+//  Created by Jason Moynihan on 10/15/25.
+//
+
+import Testing
+@testable import SwiftChat_Assistant
+
+struct SwiftChat_AssistantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
